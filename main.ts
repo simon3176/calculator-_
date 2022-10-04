4287edf9a3aa5074d3d3f0e66dd3fd9c21643574
Nomis3167 <113910266+simon3176@users.noreply.github.com>
2022-10-04 09:15:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    _1 += 1
+    basic.showNumber(_1)
+})
+input.onButtonPressed(Button.AB, function () {
+    Total = _1 + _2
+    basic.showNumber(Total)
+})
+input.onButtonPressed(Button.B, function () {
+    _2 += -1
+    basic.showNumber(_2)
+})
+let Total = 0
+let _2 = 0
+let _1 = 0
+_1 = 0
+_2 = 0
+Total = 0
+basic.forever(function () {
+	
+})
